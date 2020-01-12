0bed0ce625f58e6cf79a5b3e208411f235e51ff2
veeer <veeer@coursera.com>
2020-01-12 13:33:06 +0500

    Updates

---
+++ precache-manifest.f5890f0c2cf86265f2a3fd0b83c595ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1740cb303dc7b9c6f0970b54efeda80",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "36fd8867711d140a4659",
+    "url": "/monstersRolodex/static/css/main.4370561f.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monstersRolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersRolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36fd8867711d140a4659",
+    "url": "/monstersRolodex/static/js/main.f548cad4.chunk.js"
+  },
+  {
+    "revision": "da4a3c9888cecd880be0",
+    "url": "/monstersRolodex/static/js/runtime-main.3cfe2efc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monstersRolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
